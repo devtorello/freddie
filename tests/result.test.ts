ccca8b6536c73d09fc49423012f09602f4aa6b06
Tatiana Vitorello <vitorellotts@gmail.com>
2025-04-18 22:27:46 -0300

    :sparkles: feat: create result pattern

---
+++ tests/result.test.ts
@@ -0,0 +1,33 @@
+import { describe, it } from 'jsr:@std/testing/bdd';
+import { expect } from 'jsr:@std/expect';
+import { isOk, isError, success, failure } from '../result.ts';
+
+describe('result', () => {
+  describe('success', () => {
+    it('result.ok should be true in case of success', () => {
+      const result = success(1);
+      expect(result.ok).toBe(true);
+    });
+
+    it('result.value should be the value in case of success', () => {
+      const result = success(1);
+      if (isOk(result)) {
+        expect(result.value).toBe(1);
+      }
+    });
+  });
+  
+  describe('failure', () => {
+    it('result.ok should be false in case of failure', () => {
+      const result = failure('error');
+      expect(result.ok).toBe(false);
+    });
+
+    it('result.error should be the error in case of failure', () => {
+      const result = failure('error');
+      if (isError(result)) {
+        expect(result.error).toBe('error');
+      }
+    });
+  });
+});
